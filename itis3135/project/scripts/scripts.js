a66e2e3762529b3a76f86c3384c53c8a2172955e
baguila4 <blaguilar003@gmail.com>
2024-11-30 22:20:36 -0500

    Take out the alert

--- itis3135/project/scripts/scripts.js
+++ itis3135/project/scripts/scripts.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('nav#navbar a');
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
-            alert("Navigating to " + event.target.textContent);
         });
     });
 });
